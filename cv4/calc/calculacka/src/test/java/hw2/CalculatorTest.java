26d7121d3f57f8979916588afc7b4e19e193e086
Damirchik @dmr4eg <fifth.dmr@gmail.com>
2023-03-14 16:44:16 +0100

    adding a calculator test program

---
+++ cv4/calc/calculacka/src/test/java/hw2/CalculatorTest.java
@@ -0,0 +1,80 @@
+package hw2;
+
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.function.Executable;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class CalculatorTest {
+        //ARRANGE
+        static Calculator calc;
+        @BeforeAll
+        public static void initVariable() {
+            Calculator calc = new Calculator();
+        }
+
+        @Test
+        public void add_addsAtoB_5() {
+            //ARRANGE
+            Calculator calc = new Calculator();
+
+            //ACT
+            int num = calc.add(2, 3);
+
+            //ASSERT
+            Assertions.assertEquals(5, num);
+        }
+
+        @Test
+        public void subtract_substractsAfromB_3() {
+            //ARRANGE
+            Calculator calc = new Calculator();
+
+            //ACT
+            int num = calc.subtract(6, 3);
+
+            //ASSERT
+            Assertions.assertEquals(3, num);
+        }
+
+        @Test
+        public void multiply_multiplyAtoB_10() {
+            //ARRANGE
+            Calculator calc = new Calculator();
+
+            //ACT
+            int num = calc.multiply(5, 2);
+
+            //ASSERT
+            Assertions.assertEquals(10, num);
+        }
+
+        @Test
+        @Order(1)
+        public void divide_divisionofAtoB_4() {
+            //ARRANGE
+            Calculator calc = new Calculator();
+
+            //ACT
+            int num = calc.divide(16, 4);
+
+            //ASSERT
+            Assertions.assertEquals(4, num);
+        }
+
+        @Test
+        @Order(2)
+        public void divide_divisionofAtoB_N() {
+            //ARRANGE
+            Calculator calc = new Calculator();
+
+            // ACT + ASSERT 1
+            Exception exception = Assertions.assertThrows(Exception.class, () -> calc.divide(16, 0));
+
+            String expectedMessage = "/ by zero";
+            String actualMessage = exception.getMessage();
+
+            // ASSERT 2
+            Assertions.assertEquals(expectedMessage, actualMessage);
+        }
+
+}

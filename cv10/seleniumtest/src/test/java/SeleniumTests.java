20dda90bf16c3de9ca69b44f6c2637a97d707cac
Damirchik @dmr4eg <fifth.dmr@gmail.com>
2023-05-07 23:28:46 +0200

    idk

--- cv10/seleniumtest/src/test/java/SeleniumTests.java
+++ cv10/seleniumtest/src/test/java/SeleniumTests.java
@@ -24,6 +24,7 @@ public class SeleniumTests {
 
         driver.findElement(By.cssSelector("#AccountFrm_city")).sendKeys("Prague");
 
+        
         driver.findElement(By.cssSelector("#AccountFrm_zone_id")).sendKeys("Praha");
 
         driver.findElement(By.cssSelector("#AccountFrm_postcode")).sendKeys("12345");
